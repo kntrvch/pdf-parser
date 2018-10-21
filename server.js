2bb73c146113647102b90c5857f280d507901f72
JK <404.anerroroccurred@gmail.com>
2018-10-21 17:56:19 +0200

    flesch index (unstable)

--- server.js
+++ server.js
@@ -8,12 +8,14 @@ const pdf = require('pdf-parse');
 const tm = require('text-miner');
 const formidable = require('formidable');
 const _ = require('underscore');
-
+const Researcher = require( "yoastseo" ).Researcher;
+const Paper = require( "yoastseo" ).Paper;
 
 app.set('views', rootPath + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
+
     res.render('index', { 
         freq: null, 
         metadata: null, 
@@ -34,17 +36,18 @@ app.post('/', function (req, res) {
         console.log('Uploaded ' + file.name);
 
         let dataBuffer = fs.readFileSync('uploads/' + file.name);
+        var result;
 
         pdf(dataBuffer).then(function (data) {
 
             // number of pages
-            console.log(data.numpages);
+            //console.log(data.numpages);
             // number of rendered pages
-            console.log(data.numrender);
+            //console.log(data.numrender);
             // PDF info
-            console.log(data.info);
+            //console.log(data.info);
             // PDF metadata
-            console.log(data.metadata);
+            //console.log(data.metadata);
             // PDF.js version
             // check https://mozilla.github.io/pdf.js/getting_started/
             //console.log(data.version);
@@ -56,8 +59,8 @@ app.post('/', function (req, res) {
             corpus.addDoc(data.text);
             corpus.trim();
             corpus.clean();
-            corpus.removeInterpunctuation();
-            corpus.removeDigits();
+            //corpus.removeInterpunctuation();
+            //corpus.removeDigits();
             corpus.removeInvalidCharacters();
             corpus.stem();
 
@@ -67,14 +70,26 @@ app.post('/', function (req, res) {
 
             sortedTerms = _.filter(sortedTerms, function(term) { return term.word != ''; });
 
-            res.json({ 
+            var finalText = corpus.documents[0].text.replace(/<(?:.|\n)*?>/gm, '');
+            console.log('text: ', finalText);
+            var researcher = new Researcher( new Paper(finalText) );
+            var fleschIndex = researcher.getResearch("calculateFleschReading");
+            console.log('flesch: ', fleschIndex);
+
+            result = { 
                 filename: file.name,
                 freq: sortedTerms.reverse(), 
                 info: data.info, 
                 pages: data.numrender, 
-                chart:sortedTerms.slice(0, 10)
-            });
+                chart:sortedTerms.slice(0, 10), 
+                flesch: fleschIndex
+            };
 
+            
+
+        }).then(function() {
+            
+res.json(result);
         }).catch(function (error) {
             // handle exceptions
             console.log(error);
